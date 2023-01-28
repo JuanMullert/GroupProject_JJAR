8f405565585a7b9f267ebdd1847aed7bb3d0c3c3
muturich <calvinrich20@gmail.com>
2023-01-27 20:21:28 -0600

    touchbase on routers and handlebars

---
+++ Develop-project/controllers/api/signup_routes.js
@@ -0,0 +1,7 @@
+const { route } = require('./user_routes');
+
+const router = require('express').Router();
+
+router.post('/signup', async (req,res)=> {
+
+})
--- Develop-project/controllers/api/user_routes.js
+++ Develop-project/controllers/api/user_routes.js
@@ -2,6 +2,7 @@
 const router = require('express').Router();
 const { User } = require('../../Models');
 
+
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
