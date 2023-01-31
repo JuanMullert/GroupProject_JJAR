d607f45cb37dfcd1c6bac06c1e136bbc646f1951
muturich <calvinrich20@gmail.com>
2023-01-30 21:34:42 -0600

    routes changes on handlebars

--- Develop-project/public/js/login.js
+++ Develop-project/public/js/login.js
@@ -1,30 +1,30 @@
 const loginFormHandler = async (event) => {
   event.preventDefault();
 
-  const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
 
   if (email && password) {
-    // route to the api in use for front end
-    const response = await fetch('/api/user_routes/login', {
-      method: 'POST',
+    // route to the api in use for front end--Juan
+    const response = await fetch("../controllers/api/user_routes", {
+      method: "POST",
       body: JSON.stringify({ email, password }),
-      headers: { 'Content-Type': 'application/json' },
+      headers: { "Content-Type": "application/json" },
     });
     // direction after login
     if (response.ok) {
-      document.location.replace('/');
-    } else {
+      document.location.replace("/");
       // TODO make a route instead of provide a failed log
       // TODO make a sign up page
-       // alert('Failed to log in');
-      return 
-      
-        }
+      // alert('Failed to log in');
+      return;
+    } else {
+      document.location.replace("../controllers/api/signup_routes");
+    }
   }
 };
 
 document
-  .querySelector('.login-form')
+  .querySelector(".login-form")
   // when user clicks submit, run loginFormHandler
-  .addEventListener('submit', loginFormHandler);
+  .addEventListener("submit", loginFormHandler);
