5270836d211d0c66e580bdaa31232d4ee459712d
JuanMullert <114509238+JuanMullert@users.noreply.github.com>
2023-02-03 15:20:02 -0600

    worked on homepage.js

--- Develop-project/server.js
+++ Develop-project/server.js
@@ -31,12 +31,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
   
 
-  app.get('/', (req, res) => {
-    res.render('login');
-  });
+  // app.get('/', (req, res) => {
+  //   res.render('login');
+  // });
+
+  // app.get('/', (req, res) => {
+  //   res.render('signup');
+  // });
 
   app.get('/', (req, res) => {
-    res.render('signup');
+    res.render('homepage');
   });
 
   app.use(routes)
