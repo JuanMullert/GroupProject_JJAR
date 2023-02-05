aadfa6bfa06495f467e24234dd16313b3978b66b
JuanMullert <114509238+JuanMullert@users.noreply.github.com>
2023-02-05 12:50:44 -0600

    logout

--- Develop-project/controllers/homepage_routes.js
+++ Develop-project/controllers/homepage_routes.js
@@ -2,22 +2,19 @@ const router = require('express').Router();
 const { User, Team } = require('../Models');
 const withAuth = require('../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     if (req.session.logged_in) {
       res.render('homepage')
     }
     else{
-    res.render('login')
+    res.render('homepage')
     }
   }
   catch (err) {
     res.status(500).json(err);
   }
 });
-
-
-
 
 
 module.exports = router;
